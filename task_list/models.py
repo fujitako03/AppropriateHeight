f2071735e36511f96534f64a3392e154b03361f0
fujitako03 <thisis.snsd03@gmail.com>
2021-12-03 01:05:15 +0900

    feat __table_args__ を追加

--- task_list/models.py
+++ task_list/models.py
@@ -1,6 +1,7 @@
 from task_list import db
 
 class Task(db.Model):
+    __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text(), nullable=False)
 
