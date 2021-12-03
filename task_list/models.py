515c408e802e8b938748772891df8b7fb2cc0b70
fujitako03 <thisis.snsd03@gmail.com>
2021-12-03 22:36:57 +0900

    初期化

--- task_list/models.py
+++
@@ -1,9 +0,0 @@
-from task_list import db
-
-class Task(db.Model):
-    __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text(), nullable=False)
-
-    def __repr__(self):
-        return '<Task: {}>'.format(self.name)
